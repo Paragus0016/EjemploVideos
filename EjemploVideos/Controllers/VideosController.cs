8e5b846482f31262ffe2e5dea7684c4572798617
AGUSTIN ADOLFO MELLA ZAPATA <agustin.mella@inacapmail.cl>
2022-01-02 20:56:53 -0300

    Agregar archivos de proyecto.

---
+++ EjemploVideos/Controllers/VideosController.cs
@@ -0,0 +1,42 @@
+﻿using EjemploVideos.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploVideos.Controllers
+{
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveVideos(HttpPostedFileBase fileUpload)
+        {
+            try
+            {
+                //fileUpload
+                using (var db = new EjemploVideos.Models.EjemploBlobEntities())
+                {
+                    MemoryStream target = new MemoryStream();
+                    fileUpload.InputStream.CopyTo(target);
+                    Videos video = new Videos() { id = Guid.NewGuid(), data = target.ToArray() };
+                    db.Videos.Add(video);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { Value = false, Message = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { Value = true, Message = "Subido con éxito a BBDD" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ EjemploVideos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EjemploVideos.MvcApplication" Language="C#" %>
